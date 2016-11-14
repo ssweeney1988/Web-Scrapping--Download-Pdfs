4d2a2816c48a8a15a3ad794be2a1801d7b5f1ec3
ssweeney1988 <ssweeney@bvn.com.au>
2016-11-14 13:19:26 +1100

    Pdf's first crack

---
+++ Download Files.py
@@ -0,0 +1,33 @@
+#import urlpage
+import urllib2
+import os
+import sys
+import requests
+
+from bs4 import BeautifulSoup
+
+url= "http://www.nswrealestate.com.au/renting/tenancy-application"
+download_path= "C:\Python\mose"
+
+
+i=0
+request = urllib2.Request(url,None)
+print request
+html =  urllib2.urlopen(request)
+
+print html
+soup= BeautifulSoup(html.read(),"html.parser")
+
+for tag in soup.findAll('a', href=True): # find <a> tags with href in it so you know its for urls
+	#tag['href'] = urlparse.urljoin(url, tag['href']) 
+	#this is so if it doesnt contain the full url is it adding the url to it
+
+	if os.path.splitext(os.path.basename(tag['href']))[1] == '.pdf':
+		current= urllib2.urlopen(tag['href'])
+		print "\n[*] Downloading: %s" %(os.path.basename(tag['href']))
+
+		f = open(download_path + "\\" + os.path.basename(tag['href'], 'wb'))
+		f.write(current.read())
+		f.close()
+		i+=1
+print "\n[*] Downloaded %d files" %(i+1)
