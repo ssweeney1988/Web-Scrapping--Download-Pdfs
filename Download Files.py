da074979039eaa84c7ed92564ef6ea394b5f48f2
ssweeney1988 <samuel.n.sweeney@gmail.com>
2016-11-14 18:52:37 +1100

    New update
    working

--- Download Files.py
+++ Download Files.py
@@ -2,11 +2,11 @@
 import urllib2
 import os
 import sys
-import requests
+import requests, webbrowser
 
 from bs4 import BeautifulSoup
 
-url= "http://www.nswrealestate.com.au/renting/tenancy-application"
+url= "http://security.cs.rpi.edu/courses/binexp-spring2015/"
 download_path= "C:\Python\mose"
 
 
@@ -15,18 +15,28 @@ res.raise_for_status()  # Raises if there is an error
 soup= BeautifulSoup(res.text, "html.parser")
 
 
+#print len(res.text) 
+
 i=0
 
 for tag in soup.findAll('a', href=True): # find <a> tags with href in it so you know its for urls
 	#tag['href'] = urlparse.urljoin(url, tag['href']) 
 	#this is so if it doesnt contain the full url is it adding the url to it
-
-	if os.path.splitext(os.path.basename(tag['href']))[1] == '.pdf':
-		current= urllib2.urlopen(tag['href'])
+	#print tag
+	 if os.path.splitext(os.path.basename(tag['href']))[1] == '.pdf':
+		#print tag
+		#print os.path.basename(tag['href']
+		print tag['href']
+		print tag		
+		
 		print "\n[*] Downloading: %s" %(os.path.basename(tag['href']))
 
-		f = open(download_path + "\\" + os.path.basename(tag['href'], 'wb'))
+		f = open(download_path + "\\" + os.path.basename(tag['href']))
+		#open('C:\Python\mose\RomeoAndJuliet.txt', 'wb')
+
+		print f
 		f.write(current.read())
 		f.close()
 		i+=1
+print tag
 print "\n[*] Downloaded %d files" %(i+1)
